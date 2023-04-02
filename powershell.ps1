c8f3f05b1a5ebab8b72e3227d4c13c48bde925eb
betyar98 <89571586+betyar98@users.noreply.github.com>
2023-04-02 18:13:12 +0200

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -1,5 +1,6 @@
 #Run As Administrator
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -WindowStyle hidden -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
 
 $code = @"
     [DllImport("user32.dll")]
